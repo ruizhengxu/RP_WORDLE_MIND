8075c5463c361eb4e4b53c17f348b75cbad03630
Hakim <hakim.amraoui@outlook.fr>
2022-04-16 23:57:54 +0200

    Some tests done and courbes ploted

--- wordle_mind.py
+++ wordle_mind.py
@@ -1,4 +1,8 @@
-import random, string, time, copy
+import random
+import string
+import time
+import copy
+import csv
 from difflib import get_close_matches
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,17 +20,20 @@ this class takes below inputs :
         list of all available words
     - secretWord : str
 """
+
+
 class CSP:
-    
+
     def __init__(self, dico: list, secretWord: str):
         self.dico = dico
         self.secretWord = secretWord
         self.vars = dict.fromkeys([i for i in range(len(self.secretWord))])
-        self.domains = [list(string.ascii_lowercase) for _ in range(len(self.secretWord))]
-        self.n = 0 # Number of tested word
-    
+        self.domains = [list(string.ascii_lowercase)
+                        for _ in range(len(self.secretWord))]
+        self.n = 0  # Number of tested word
+
     ##########################################################################
-    
+
     """
     Backtracking method consists to initialize each variables with potentials values
     then check if the current solution is cosistent, if not, it intialize with next values and so on,
@@ -79,9 +86,9 @@ class CSP:
             if consistent:
                 return True
         return False
-                
-    ##########################################################################            
-    
+
+    ##########################################################################
+
     """
     Forward Checking (FC) method consists to reduce domain of each variables until find the solution.
     """
@@ -145,13 +152,12 @@ class CSP:
         for word in reduced_dico:
             if word[x_k] == v and word[x_j] == v_:
                 return True
-        return False    
-    
+        return False
+
     ##########################################################################
-    
+
     def toWord(self, instance: dict):
         return ''.join([c for c in instance.values()])
-
 
 
 ###################################################
@@ -159,7 +165,6 @@ class CSP:
 # Solver with Genetic Algorithm
 #
 ###################################################
-
 """
 Solver based on Genetic Algorithm
 this class takes below inputs :
@@ -173,33 +178,35 @@ this class takes below inputs :
         maximum of generation of the algorithm, the algo stops when the number
         of generation reaches max_gen
 """
+
+
 class GeneticAlgorithm:
-    
+
     def __init__(self, dico: list, secretWord: str, max_size: int, max_gen: int, popsize: int):
         self.dico = [w for w in dico if len(w) == len(secretWord)]
         self.secretWord = secretWord
         self.max_size = max_size
         self.max_gen = max_gen
         self.popsize = popsize
-    
-    ##########################################################################    
-    
+
+    ##########################################################################
+
     def selection(self, population: list, fitness: list):
         tmp_fitness = copy.deepcopy(fitness)
         tmp_population = copy.deepcopy(population)
-        
+
         pos = self.bestInd(tmp_fitness)
         ind1 = tmp_population[pos]
         tmp_fitness.pop(pos)
         tmp_population.pop(pos)
         ind2 = tmp_population[self.bestInd(tmp_fitness)]
-        
+
         return ind1, ind2
-    
+
     def crossover(self, ind1: str, ind2: str):
         child1 = ""
         child2 = ""
-        
+
         for i in range(len(ind1)):
             pb = random.random()
             if pb > 0.5:
@@ -208,58 +215,59 @@ class GeneticAlgorithm:
             else:
                 child1 += ind2[i]
                 child2 += ind1[i]
-        
+
         child1 = self.getClosestWord(child1)
         child2 = self.getClosestWord(child2)
-        
+
         return child1, child2
-    
+
     def mutation(self, ind):
         fit = self.evaluate(ind)
-        if len(self.secretWord) - fit[0] == 1: # replace
+        if len(self.secretWord) - fit[0] == 1:  # replace
             tmp_char = random.choice(list(string.ascii_lowercase))
             pos = random.randint(0, len(ind))
             ind = ind[:pos] + tmp_char + ind[pos+1:]
-        else: # swap
+        else:  # swap
             pos = random.choices([i for i in range(len(ind))], k=2)
             tmp_ind = list(ind)
             tmp_ind[pos[0]], tmp_ind[pos[1]] = tmp_ind[pos[1]], tmp_ind[pos[0]]
             ind = "".join(tmp_ind)
-        
+
         return self.getClosestWord(ind)
 
     def isCompatible(self, ind, fitness):
         f = self.evaluate(ind)
-        
+
         for fit in fitness:
             if f[0] <= fit[0] and f[1] <= fit[1]:
                 return False
-        
+
         return True
-    
+
     def bestInd(self, fitness):
         pos = 0
         bestFit = fitness[pos][0] * 2 + fitness[pos][1]
-        
+
         for i in range(1, len(fitness)):
             currentFit = fitness[i][0] * 2 + fitness[i][1]
             if currentFit > bestFit:
                 bestFit = currentFit
                 pos = i
-                
+
         return pos
-    
+
     def getClosestWord(self, word):
         matched_words = get_close_matches(word, self.dico)
         if len(matched_words) == 0:
             return ""
         return matched_words[self.bestInd([self.evaluate(w) for w in matched_words])]
-    
+
     """
     Evaluate return fitness for the word which we want to evaluate.
     Returned fitness contains two numbers (number of well placed words, number of misplaced words)
-    
+
     """
+
     def evaluate(self, word: str):
         well_placed = 0
         misplaced = 0
@@ -275,36 +283,37 @@ class GeneticAlgorithm:
                 well_placed += 1
                 tmp_secretWord = tmp_secretWord[:i] + tmp_secretWord[i+1:]
                 tmp_word = tmp_word[:i] + tmp_word[i+1:]
-        
+
         # Count misplaced words
         for letter in tmp_word:
             if letter in tmp_secretWord:
                 misplaced += 1
                 tmp_secretWord = tmp_secretWord.replace(letter, "")
-        
+
         return [well_placed, misplaced]
-    
+
     def solve(self, verbose=False):
-        
+
         # randomly initialize a set of words
-        population = random.choices([w for w in dico if len(w) == len(self.secretWord)], k=self.popsize)
+        population = random.choices(
+            [w for w in dico if len(w) == len(self.secretWord)], k=self.popsize)
         fitness = [self.evaluate(ind) for ind in population]
         gen = 0
-        
+
         pos = self.bestInd(fitness)
         bestFit = fitness[pos]
-        
+
         # if the secret word is already in first population
         if population[pos] == self.secretWord:
             print("Gen :", gen)
             print("All population :", population)
             print("All fitness :", fitness)
             return population[pos], bestFit
-        
+
         for _ in range(self.max_gen):
             gen += 1
             new_population = []
-            
+
             for _ in range(int(self.popsize/2)):
                 ind1, ind2 = self.selection(population, fitness)
                 child1, child2 = self.crossover(ind1, ind2)
@@ -316,7 +325,7 @@ class GeneticAlgorithm:
                     child2 = self.mutation(child2)
                     if child2 not in new_population and self.isCompatible(child2, fitness):
                         new_population.append(child2)
-                        
+
             if len(new_population) > 0:
                 population += new_population.copy()
                 fitness += [self.evaluate(ind) for ind in new_population]
@@ -324,26 +333,120 @@ class GeneticAlgorithm:
                 if fitness[i] > bestFit:
                     pos = i
                     bestFit = fitness[i]
-            
+
             if population[pos] == self.secretWord:
                 print("Gen :", gen)
                 print("All population :", population)
                 print("All fitness :", fitness)
                 return population[pos], bestFit
-            
+
             if len(population) == self.max_size:
                 print("Max size of population reached.")
                 print("Gen :", gen)
                 print("All population :", population)
                 print("All fitness :", fitness)
                 return population[pos], bestFit
-        
+
         print("Max gen reached.")
         print("All population :", population)
         print("All fitness :", fitness)
         return population[pos], bestFit
 
-    
+###################################################
+#
+# Test
+#
+###################################################
+
+def test_4_letters():
+    avrg_time_BT, avrg_time_FC = [], []
+    f = open('Tests/src/4_letters', 'w')
+    writer = csv.writer(f)
+    head = ['Iteration', 'Word', 'Time Back Track (in sec)', 'Time Forward Checking (in sec)']
+    writer.writerow(head)
+    for i in range(20):
+            print(f'\nStart of the {i} iterations.')
+            secretWord = choose_secretWord(dico, 4)
+            csp = CSP(dico, secretWord)
+
+            body = [i, secretWord]
+            print(f'The secret word is {secretWord}.')
+
+            print("Running back tracking method : ....")
+            start = time.time()
+            _ = csp.backTracking(verbose=True)
+            stop = time.time()
+            # print(f'Time : {stop - start}')
+            body.append(stop - start)
+
+            print("\nRunning back tracking method with Forward checking : ....")
+            start = time.time()
+            _ = csp.forwardChecking(verbose=True)
+            stop = time.time()
+            # print(f'Time : {stop - start}')
+            body.append(stop - start)
+
+
+            writer.writerow(body)
+
+
+    f.close()
+
+
+def test_n_words(n: int, nb_letter_min: int, nb_letter_max: int):
+    avrg_time_BT, avrg_time_FC = [], []
+    f = open('Tests/src/4_letters', 'w')
+    writer = csv.writer(f)
+
+    for l in range(nb_letter_min, nb_letter_max):
+        print(f'Words of {l} letters')
+        time_BT, time_FC = [], []
+        for i in range(n):
+            print(f'\nStart of the {i} iterations.')
+            secretWord = choose_secretWord(dico, l)
+            csp = CSP(dico, secretWord)
+
+            print(f'The secret word is {secretWord}.')
+
+            print("Running back tracking method : ....")
+            start = time.time()
+            _ = csp.backTracking(verbose=True)
+            stop = time.time()
+            # print(f'Time : {stop - start}')
+            time_BT.append(stop - start)
+
+            print("\nRunning back tracking method with Forward checking : ....")
+            start = time.time()
+            _ = csp.forwardChecking(verbose=True)
+            stop = time.time()
+            # print(f'Time : {stop - start}')
+            time_FC.append(stop - start)
+
+        # print(f'Time list BT: {time_BT}')
+        # print(f'avrg_time_BT : {np.mean(time_BT)}')
+        avrg_time_BT.append(np.mean(time_BT))
+
+        # print(f'Time list FC: {time_FC}')
+        # print(f'avrg_time_FC : {np.mean(time_FC)}')
+        avrg_time_FC.append(np.mean(time_FC))
+
+    # First figure for Back Tracking
+    plt.figure()
+    plt.plot(np.arange(nb_letter_min, nb_letter_max),
+             avrg_time_BT, color='m', label='Back Tracking')
+    plt.plot(np.arange(nb_letter_min, nb_letter_max),
+             avrg_time_FC, color='c', label='Forward Checking')
+    plt.xlabel("Length of words")
+    plt.ylabel("Time in sec")
+    plt.legend()
+    plt.title(
+        "Average time to solve the problem depending on the length of words.")
+    plt.savefig(f"Tests/img/{nb_letter_max - 1}_letters")
+
+
+    f.close()
+
+
 
 ###################################################
 #
@@ -351,43 +454,49 @@ class GeneticAlgorithm:
 #
 ###################################################
 
+
 def read_dico(path: str):
     with open(path) as f:
         dico = f.read().splitlines()
     return dico
 
+
 def choose_secretWord(dico, N):
     return random.choice([w for w in dico if len(w) == N])
+
 
 if __name__ == '__main__':
     # Initialize
     file_path = "./dico.txt"
     dico = read_dico(file_path)
     # length of secretWord = [2, 22] !!!
-    secretWord = choose_secretWord(dico, 5)
-    print("Secret word is :", secretWord)
+    # secretWord = choose_secretWord(dico, 5)
+    # print("Secret word is :", secretWord)
 
     # Solve with CSP
     # csp = CSP(dico, secretWord)
-    
+
     # print("Running back tracking method : ....")
     # start = time.time()
     # w = csp.backTracking(verbose=True)
     # stop = time.time()
     # print("Word found with BackTracking :", w, "in", stop-start ,"seconds")
-    
+
     # print("\nRunning forward checking method : ....")
     # start = time.time()
     # w = csp.forwardChecking(verbose=True)
     # stop = time.time()
     # print("Word found with Forward checking :", w, "in", stop-start ,"seconds")
-    
+
     # Solve with Genetic Algorithm
-    ga = GeneticAlgorithm(dico, secretWord, max_size=100, max_gen=100, popsize=10)
-    
-    print("\nRunning genetic algorithm : ....")
-    start = time.time()
-    w, fit = ga.solve(verbose=True)
-    stop = time.time()
-    print("Word found with Forward checking :", w, "in", stop-start ,"seconds")
-    
+    # ga = GeneticAlgorithm(dico, secretWord, max_size=100,
+    #                       max_gen=100, popsize=10)
+
+    # print("\nRunning genetic algorithm : ....")
+    # start = time.time()
+    # w, fit = ga.solve(verbose=True)
+    # stop = time.time()
+    # print("Word found with Forward checking :", w, "in", stop-start, "seconds")
+
+    # test_n_words(20, 4, 5)
+    # test_4_letters()
